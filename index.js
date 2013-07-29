18540f6c06f0a85f63729745d7234523bd8be5f9
Lee Treveil <leetreveil@gmail.com>
2013-07-29 13:33:48 +0100

    cleanup

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ var util = require('util');
 var tapConv = require('tap-test-converter');
 var program = require('commander');
 var pkg = require('./package');
-var StreamSplitter = require("stream-splitter");
+var streamSplitter = require('stream-splitter');
 var http = require('http');
 var static = require('node-static');
+var fmt = util.format;
 
 var ids = {}; // used to track the test runs
 var testResults = [];
@@ -17,6 +18,7 @@ var tunnel;
 var inShutdown = false;
 var resultCollectorPort = 1942;
 var portRange = 45032;
+var defaultCfgFilePath = './config.json';
 
 program
     .version(pkg.version)
@@ -29,7 +31,7 @@ program
     .option('-v, --verbose', 'output debugging information')
     .parse(process.argv);
 
-var config = require(program.config || './config.json');
+var config = require(program.config || defaultCfgFilePath);
 config.bs_username = program.bs_username || config.bs_username;
 config.bs_password = program.bs_password || config.bs_password;
 config.bs_key = program.bs_key || config.bs_key;
@@ -37,24 +39,72 @@ config.test_file = program.testfile || config.test_file;
 config.directory = program.directory || config.directory;
 for (var key in config) {
     if (!config[key]) {
-        throw new Error(key + ' not set in config or cli arguments');
+        throw new Error(fmt('%s not set in config or cli arguments', key));
     }
 }
 
-function log (msg) {
-    if (program.verbose) console.log(msg);
-}
-
-// setup browserstack
 var client = browserstack.createClient({
     username: config.bs_username,
     password: config.bs_password
 });
 
+async.parallel({
+    'fileServer' : function (callback) {
+        getPort(function (port) {
+            http.createServer(function (req, res) {
+                req.addListener('end', function () {
+                    new static.Server(config.directory).serve(req, res, function (e, rsp) {
+                        log(fmt('[%d]: %s', res.statusCode, req.url));
+                    });
+                }).resume();
+            }).listen(port, function () {
+                log(fmt('started fileserver on port %d', port));
+                callback(null, port);
+            });
+        });
+    },
+    'resultCollector' : function (callback) {
+        touchstone.createServer().listen(resultCollectorPort, function () {
+            callback(null, resultCollectorPort); // TODO: have we ignored errors here?
+        }).on('result', processTestResult);
+    }
+}, function (err, ports) {
+    var tunnelPorts = fmt.apply(this, ['localhost,%d,0,localhost,%d,0'].concat(values(ports)));
+
+    // start tunnel
+    tunnel = spawn('java', ['-jar', 'ext/BrowserStackTunnel.jar', config.bs_key, tunnelPorts]);
+    if (program.verbose) {
+        tunnel.stdout.pipe(process.stdout);
+        tunnel.stderr.pipe(process.stderr);
+    }
+
+    var splitter = tunnel.stdout.pipe(streamSplitter('\n'));
+    splitter.encoding = 'utf8';
+    splitter.on('token', function (token) {
+        var expected = 'You can now access your local server(s) in our remote browser';
+        if (token.substring(0, 61) === expected) {
+            log('tunnel started successfully!!');
+            launchBrowsers(ports.fileServer);
+        }
+    });
+    splitter.on('done', function () {
+        if (!inShutdown) {
+            console.log('ERROR: browserstack tunnel failed to start, ' +
+                                'see --verbose output for more info');
+            process.exit(1);
+        }
+    });
+});
+
+function log (msg) {
+    if (program.verbose) {
+        console.log(msg);
+    }
+}
+
 function getPort (cb) {
     var port = portRange;
     portRange += 1;
-
     var server = net.createServer();
     server.listen(port, function (err) {
         server.once('close', function () {
@@ -62,7 +112,6 @@ function getPort (cb) {
         });
         server.close();
     });
-
     server.on('error', function (err) {
         getPort(cb);
     });
@@ -87,9 +136,7 @@ function launchBrowsers (port) {
         ids[id] = {};
         ids[id].instance = browser;
 
-        var url = util.format('http://localhost:%s/%s?id=%s',
-            port, config.test_file, id);
-        browser.url = url;
+        browser.url = fmt('http://localhost:%s/%s?id=%s', port, config.test_file, id);
 
         log('launching: ' + JSON.stringify(browser));
 
@@ -105,10 +152,12 @@ function launchBrowsers (port) {
 function processTestResult (id, result) {
     testResults.push(result);
     var instance = ids[id].instance;
-    var browser = util.format('%s %s (%s)', instance.browser, instance.version, instance.os);
-    console.log('# START -- ' + browser + ' ----------');
+    var browser = fmt('%s %s (%s)', instance.browser, instance.version, instance.os);
+
+    console.log(fmt('# START -- %s ----------', browser));
     console.log(tapConv(result));
-    console.log('# END ---- ' + browser + ' ----------');
+    console.log(fmt('# END ---- %s ----------', browser));
+
     if (ids[id]) {
         client.terminateWorker(ids[id].worker_id, function (err, data) {
             if (err) throw err;
@@ -117,7 +166,7 @@ function processTestResult (id, result) {
             if (Object.keys(ids).length == 0) {
                 var totalFailed = testResults.map(function (x) { return x.failed })
                                              .reduce(function (a, b) { return a + b });
-                console.log(util.format('# out of %d test runs, %d failed', 
+                console.log(fmt('# out of %d test runs, %d failed',
                     testResults.length, totalFailed));
                 process.exit((totalFailed > 0 ? 1 : 0));
             }
@@ -125,61 +174,15 @@ function processTestResult (id, result) {
     }
 }
 
-async.parallel({
-    'fileServer' : function (callback) {
-        getPort(function (port) {
-            var fileServer = new static.Server(config.directory);
-            http.createServer(function (req, res) {
-                req.addListener('end', function () {
-                    fileServer.serve(req, res, function (e, rsp) {
-                        log('[' + res.statusCode + ']: ' + req.url);
-                    });
-                }).resume();
-            }).listen(port, function () {
-                callback(null, port);
-            });
-        });
-    },
-    'resultCollector' : function (callback) {
-        touchstone.createServer().listen(resultCollectorPort, function () {
-            callback(null, resultCollectorPort); // TODO: have we ignored errors here?
-        }).on('result', processTestResult);
-    }
-}, function (err, ports) {
-    var tunnelPorts = util.format.apply(this,
-                        ['localhost,%d,0,localhost,%d,0'].concat(values(ports)));
-
-    // start tunnel
-    tunnel = spawn('java', ['-jar',
-                            'ext/BrowserStackTunnel.jar',
-                            config.bs_key,
-                            tunnelPorts]);
-    if (program.verbose) tunnel.stdout.pipe(process.stdout); 
-    if (program.verbose) tunnel.stderr.pipe(process.stderr);
-
-    var splitter = tunnel.stdout.pipe(StreamSplitter('\n'));
-    splitter.encoding = 'utf8';
-    splitter.on('token', function (token) {
-        var expected = 'You can now access your local server(s) in our remote browser';
-        if (token.substring(0, 61) === expected) {
-            log('tunnel started successfully!!');
-            launchBrowsers(ports.fileServer);
-        }
-    });
-    splitter.on('done', function () {
-        if (!inShutdown) {
-            console.log('ERROR: browserstack tunnel failed to start, ' +
-                                'see --verbose output for more info');
-            process.exit(1);
-        }
-    });
-});
-
 function exit (exitCode) {
     inShutdown = true;
     process.kill(tunnel);
-    var workerIds = values(ids).map(function (item) {return item.worker_id});
-    if (workerIds.length > 0) log('Stopping' + ' ' + workerIds.join(', '));
+    var workerIds = values(ids).map(function (item) {
+        return item.worker_id;
+    });
+    if (workerIds.length > 0) {
+        log(fmt('Stopping browserstack workers: %s', workerIds.join(', ')));
+    }
     async.each(workerIds, client.terminateWorker.bind(client), function (err) {
         process.exit(exitCode);
     });
